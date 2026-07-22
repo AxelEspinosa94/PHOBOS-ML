e0ef4dc4fe1244eaf5051bf09b0ef3ff1211bd27
Ayrton <caer23.voltr@outlook.com>
2026-07-22 06:33:07 -0600

    Added compre method to tensor.c

--- tests/test_tensor.c
+++ tests/test_tensor.c
@@ -1,5 +1,6 @@
-#include "tensor.h"
 #include <stdio.h>
+
+#include "tensor.h"
 
 int main(void) {
     printf("=== PHOBOS-ML Tensor Test ===\n");
@@ -32,6 +33,36 @@ int main(void) {
 
     printf("Data filled. First element: %.2f\n", data[0]);
     printf("Last element: %.2f\n", data[t->size - 1]);
+
+    printf("=== Testing tensor_compare ===\n");
+
+    tensor_t* a = tensor_create(DTYPE_FLOAT32, 2, shape);
+    tensor_t* b = tensor_create(DTYPE_FLOAT32, 2, shape);
+
+    float* ad = (float*)a->data;
+    float* bd = (float*)b->data;
+
+    for (size_t i = 0; i < a->size; ++i) {
+        ad[i] = (float)i;
+        bd[i] = (float)i;
+    }
+
+    if (tensor_compare(a, b)) {
+        printf("tensor_compare: OK (tensors are equal)\n");
+    } else {
+        printf("tensor_compare: FAIL (tensors should be equal)\n");
+    }
+
+    bd[0] = 999.0f;
+
+    if (!tensor_compare(a, b)) {
+        printf("tensor_compare: OK (difference detected)\n");
+    } else {
+        printf("tensor_compare: FAIL (should detect difference)\n");
+    }
+
+    tensor_free(a);
+    tensor_free(b);
 
     // Free tensor
     tensor_free(t);
